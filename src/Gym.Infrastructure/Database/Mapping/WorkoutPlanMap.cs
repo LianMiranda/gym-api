a09a8a454520fad9dc557ed3f378a2e9642e0651
Lian Souza Miranda Mendes <lianmiranda26@gmail.com>
2025-11-14 16:29:32 -0400

    Feat(WorkoutPlan Mapping): mapping Workout Plan table

---
+++ src/Gym.Infrastructure/Database/Mapping/WorkoutPlanMap.cs
@@ -0,0 +1,67 @@
+using Gym.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Gym.Infrastructure.Database.Mapping;
+
+public class WorkoutPlanMap : IEntityTypeConfiguration<WorkoutPlan>
+{
+    public void Configure(EntityTypeBuilder<WorkoutPlan> builder)
+    {
+        builder
+            .ToTable("workout_plan");
+
+        builder
+            .HasKey(w => w.Id);
+
+        builder
+            .Property(w => w.Id)
+            .IsRequired(true)
+            .HasColumnName("id")
+            .HasColumnType("uniqueidentifier");
+        
+        builder
+            .Property(w => w.UserId)
+            .IsRequired(true)
+            .HasColumnName("user_id")
+            .HasColumnType("uniqueidentifier");
+        
+        builder
+            .Property(w => w.Name)
+            .IsRequired(true)
+            .HasColumnName("name")
+            .HasColumnType("nvarchar")
+            .HasMaxLength(100);
+        
+        builder
+            .Property(w => w.Description)
+            .IsRequired(false)
+            .HasColumnName("description")
+            .HasColumnType("text");
+        
+        builder
+            .Property(w => w.DaysPerWeek)
+            .IsRequired(false)
+            .HasColumnName("days_per_week")
+            .HasColumnType("tinyint");
+        
+        builder
+            .Property(w => w.Months)
+            .IsRequired(false)
+            .HasColumnName("months")
+            .HasColumnType("tinyint");
+        
+        builder
+            .Property(w => w.Goal)
+            .IsRequired(false)
+            .HasColumnName("goal")
+            .HasConversion<string>()
+            .HasColumnType("varchar")
+            .HasMaxLength(15);
+
+        builder
+            .HasOne(w => w.User)
+            .WithMany(u => u.WorkoutPlans)
+            .HasForeignKey(w => w.UserId);
+    }
+}
