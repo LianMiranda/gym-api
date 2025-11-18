1e46475bc0e362629dd5ce8bb9ad959c42e5f998
Lian Souza Miranda Mendes <lianmiranda26@gmail.com>
2025-11-18 15:51:58 -0400

    :sparkles: Feat(repository): Add user repository

---
+++ src/Gym.Domain/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+using Gym.Domain.Entities;
+
+namespace Gym.Domain.Interfaces.Repositories;
+
+public interface IUserRepository
+{
+    Task SaveAsync(User user);
+    void Delete(User user);
+    void Update(User user);
+    Task<User?> GetByIdAsync(Guid id);
+    Task<IEnumerable<User>?> GetAllAsync();
+}
---
+++ src/Gym.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,41 @@
+using Gym.Domain.Entities;
+using Gym.Domain.Interfaces.Repositories;
+using Gym.Infrastructure.Database.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gym.Infrastructure.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private readonly AppDbContext _context;
+
+    public UserRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task SaveAsync(User user)
+    {
+        await _context.Users.AddAsync(user);
+    }
+
+    public void Delete(User user)
+    {
+        _context.Users.Remove(user);
+    }
+
+    public void Update(User user)
+    {
+        _context.Entry<User>(user).State = EntityState.Modified;
+    }
+
+    public async Task<User?> GetByIdAsync(Guid id)
+    {
+        return await _context.Users.AsNoTracking().SingleOrDefaultAsync(user => user.Id == id);
+    }
+
+    public async Task<IEnumerable<User>?> GetAllAsync()
+    {
+        return await _context.Users.AsNoTracking().ToListAsync();
+    }
+}
