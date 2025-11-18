76844d5e19113e46865f2bfef6a5d3407f778310
Lian Souza Miranda Mendes <lianmiranda26@gmail.com>
2025-11-18 17:58:53 -0400

    :sparkles: Feat(Application): Add user dtos mapper

---
+++ src/Gym.Application/MappingImplementation/UserMapper.cs
@@ -0,0 +1,28 @@
+using Gym.Application.Dtos.User;
+using Gym.Domain.Entities;
+
+namespace Gym.Application.MappingImplementation;
+
+public static class UserMapper
+{
+    public static UserResponse MapToDto(this User user)
+    {
+        return new UserResponse
+        {
+            Id = user.Id,
+            FirstName = user.FirstName,
+            LastName = user.LastName,
+            Email = user.Email
+        };
+    }
+
+    public static User MapToEntity(this CreateUserRequest createDto, string passwordHash)
+    {
+        return new User(
+            createDto.FirstName,
+            createDto.LastName,
+            createDto.Email,
+            passwordHash
+        );
+    }
+}
