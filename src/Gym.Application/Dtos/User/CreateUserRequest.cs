c0251c3e4172a3ab598045354d2292797c7c0474
Lian Souza Miranda Mendes <lianmiranda26@gmail.com>
2025-11-19 16:19:12 -0400

    :sparkles: Feat(Application): Add simple validations with data annotations.

--- src/Gym.Application/Dtos/User/CreateUserRequest.cs
+++ src/Gym.Application/Dtos/User/CreateUserRequest.cs
@@ -1,11 +1,24 @@
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace Gym.Application.Dtos.User;
 
 public record CreateUserRequest
 {
-    [JsonPropertyName("first_name")] public string FirstName { get; set; } = null!;
-    [JsonPropertyName("last_name")] public string LastName { get; set; } = null!;
-    [JsonPropertyName("email")] public string Email { get; set; } = null!;
-    [JsonPropertyName("password")] public string Password { get; set; } = null!;
+    [JsonPropertyName("first_name")]
+    [Required]
+    public string FirstName { get; set; } = null!;
+
+    [JsonPropertyName("last_name")]
+    [Required]
+    public string LastName { get; set; } = null!;
+
+    [JsonPropertyName("email")]
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = null!;
+
+    [JsonPropertyName("password")]
+    [Required]
+    public string Password { get; set; } = null!;
 };
