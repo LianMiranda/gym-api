c47e5d6232f666f738220b3be2fdcfda7440bc99
Lian Souza Miranda Mendes <lianmiranda26@gmail.com>
2025-11-19 19:20:32 -0400

    :sparkles: Feat(application): Add "delete user" to the user controller + logger.

--- src/Gym.Api/Controllers/UsersController.cs
+++ src/Gym.Api/Controllers/UsersController.cs
@@ -10,11 +10,16 @@ public class UsersController : ControllerBase
 {
     private readonly CreateUserUseCase _createUserUseCase;
     private readonly GetAllUsersUseCase _getAllUsersUseCase;
+    private readonly DeleteUserUseCase _deleteUserUseCase;
+    private readonly ILogger<UsersController> _logger;
 
-    public UsersController(CreateUserUseCase createUserUseCase, GetAllUsersUseCase getAllUsersUseCase)
+    public UsersController(CreateUserUseCase createUserUseCase, GetAllUsersUseCase getAllUsersUseCase,
+        DeleteUserUseCase deleteUserUseCase, ILogger<UsersController> logger)
     {
         _createUserUseCase = createUserUseCase;
         _getAllUsersUseCase = getAllUsersUseCase;
+        _deleteUserUseCase = deleteUserUseCase;
+        _logger = logger;
     }
 
     [HttpPost]
@@ -27,15 +32,18 @@ public class UsersController : ControllerBase
             if (!user.IsSuccess)
                 return Conflict(user);
 
-            return Ok(user);
+            return Created("", user);
         }
         catch (Exception e)
         {
-            Console.WriteLine(e);
-            throw;
+            _logger.LogError(e, "Unexpected error while creating user.");
+            return StatusCode(
+                StatusCodes.Status500InternalServerError,
+                new { message = "An unexpected error occurred" }
+            );
         }
     }
-    
+
     [HttpGet]
     public async Task<IActionResult> GetAllUsersAsync([FromQuery] int page = 1, [FromQuery] int take = 20)
     {
@@ -50,8 +58,34 @@ public class UsersController : ControllerBase
         }
         catch (Exception e)
         {
-            Console.WriteLine(e);
-            throw;
+            _logger.LogError(e, "Unexpected error while searching for users.");
+            return StatusCode(
+                StatusCodes.Status500InternalServerError,
+                new { message = "An unexpected error occurred" }
+            );
+        }
+    }
+
+    [Route("{id}")]
+    [HttpDelete]
+    public async Task<IActionResult> DeleteUserAsync([FromRoute] Guid id)
+    {
+        try
+        {
+            var result = await _deleteUserUseCase.ExecuteAsync(id);
+
+            if (!result.IsSuccess)
+                return NotFound(result);
+
+            return NoContent();
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Unexpected error while deleting user {UserId}", id);
+            return StatusCode(
+                StatusCodes.Status500InternalServerError,
+                new { message = "An unexpected error occurred" }
+            );
         }
     }
 }
