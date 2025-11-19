4b3b43ce0fbe6e449820a173574ce4991abe3d6f
Lian Souza Miranda Mendes <lianmiranda26@gmail.com>
2025-11-19 17:55:01 -0400

    :sparkles: Feat(Application): Add the "Get all users" method to the users controller.

--- src/Gym.Api/Controllers/UsersController.cs
+++ src/Gym.Api/Controllers/UsersController.cs
@@ -1,4 +1,4 @@
-using Gym.Application.Dtos.User;
+using Gym.Application.Dtos.User.Request;
 using Gym.Application.UseCases.User;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,11 +8,13 @@ namespace Gym.Api.Controllers;
 [ApiController]
 public class UsersController : ControllerBase
 {
-    private CreateUserUseCase _createUserUseCase;
+    private readonly CreateUserUseCase _createUserUseCase;
+    private readonly GetAllUsersUseCase _getAllUsersUseCase;
 
-    public UsersController(CreateUserUseCase createUserUseCase)
+    public UsersController(CreateUserUseCase createUserUseCase, GetAllUsersUseCase getAllUsersUseCase)
     {
         _createUserUseCase = createUserUseCase;
+        _getAllUsersUseCase = getAllUsersUseCase;
     }
 
     [HttpPost]
@@ -24,6 +26,25 @@ public class UsersController : ControllerBase
 
             if (!user.IsSuccess)
                 return Conflict(user);
+
+            return Ok(user);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetAllUsersAsync([FromQuery] int page = 1, [FromQuery] int take = 20)
+    {
+        try
+        {
+            var user = await _getAllUsersUseCase.ExecuteAsync(page, take);
+
+            if (!user.IsSuccess)
+                return NotFound(user);
 
             return Ok(user);
         }
