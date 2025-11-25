d9f33865ad77bb11bd4b77a963c2ea626debda38
Lian Souza Miranda Mendes <lianmiranda26@gmail.com>
2025-11-24 21:42:16 -0400

    :sparkles: Feat(Api): authentication and authorization settings

--- src/Gym.Api/Program.cs
+++ src/Gym.Api/Program.cs
@@ -1,27 +1,52 @@
+using System.Text;
 using Gym.Api.Filters;
 using Gym.Application.Config;
 using Gym.Infrastructure.Config;
 using Gym.Infrastructure.Database.Context;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 
-builder.Services.Configure<ApiBehaviorOptions>(options =>
-{
-    options.SuppressModelStateInvalidFilter = true;
-});
+builder.Services.Configure<ApiBehaviorOptions>(options => { options.SuppressModelStateInvalidFilter = true; });
 
-builder.Services.AddControllers(options =>
-{
-    options.Filters.Add<ValidationResultFilter>();
-});
+builder.Services.AddControllers(options => { options.Filters.Add<ValidationResultFilter>(); });
 
 var connectionString = builder.Configuration.GetConnectionString("DbConnection");
 
-builder.Services.AddDbContext<AppDbContext>(options => { options.UseSqlServer(connectionString, x => x.MigrationsAssembly("Gym.Infrastructure")); });
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(connectionString, x => x.MigrationsAssembly("Gym.Infrastructure"));
+});
+
+var secret = builder.Configuration["Jwt:Secret"] ?? throw new ArgumentException("Invalid token secret key");
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme; 
+}).AddJwtBearer(options =>
+{
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = false; //"false" apenas em desenvolvimento
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ClockSkew = TimeSpan.Zero,
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret))
+    };
+});
+
 builder.Services.AddInfrastructure();
 builder.Services.AddApplication();
 
