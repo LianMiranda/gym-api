8d4589ed05a788195dd10afaa1c492e20c2b4980
Lian Souza Miranda Mendes <lianmiranda26@gmail.com>
2025-11-24 22:09:08 -0400

    :sparkles: Feat(Application auth service): Add register method to the application's auth service

--- src/Gym.Application/Services/Auth/AuthService.cs
+++ src/Gym.Application/Services/Auth/AuthService.cs
@@ -1,6 +1,8 @@
 using System.Security.Claims;
 using Gym.Application.Dtos.Auth.Request;
 using Gym.Application.Dtos.Auth.Response;
+using Gym.Application.Dtos.User.Request;
+using Gym.Application.Services.User;
 using Gym.Domain.Abstractions.ResultPattern;
 using Gym.Domain.Interfaces.Repositories;
 using Gym.Domain.Interfaces.Services;
@@ -14,16 +16,18 @@ public class AuthService : IAuthService
     private readonly IPasswordHasher _passwordHasher;
     private readonly ITokenService _tokenService;
     private readonly IUserRepository _userRepository;
+    private readonly IUserService _userService;
 
-    public AuthService(IPasswordHasher passwordHasher, ITokenService tokenService, IUserRepository userRepository)
+    public AuthService(IPasswordHasher passwordHasher, ITokenService tokenService, IUserRepository userRepository, IUserService userService)
     {
         _passwordHasher = passwordHasher;
         _tokenService = tokenService;
         _userRepository = userRepository;
+        _userService = userService;
     }
 
 
-    public async Task<Result<LoginResponse>> Login(LoginRequest request, CancellationToken cancellationToken)
+    public async Task<Result<LoginResponse>> LoginAsync(LoginRequest request, CancellationToken cancellationToken)
     {
         var user = await _userRepository.GetByEmailAsync(request.Email);
 
@@ -52,4 +56,30 @@ public class AuthService : IAuthService
         return Result<LoginResponse>.Success(response);
     }
     
+
+    public async Task<Result<LoginResponse>> RegisterAsync(CreateUserRequest request, CancellationToken cancellationToken)
+    {
+        var result = await _userService.CreateAsync(request, cancellationToken);
+        
+        if(!result.IsSuccess)
+            return Result<LoginResponse>.Error(result.Message);
+        
+        var user = result.Data!;
+        
+        var claims = new List<Claim>
+        {
+            new(ClaimTypes.Email, user.Email),
+            new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new(JwtRegisteredClaimNames.Sub, user.Id.ToString())
+        };
+
+        var token = _tokenService.GenerateToken(claims, cancellationToken);
+
+        var response = new LoginResponse
+        {
+            Token = token
+        };
+
+        return Result<LoginResponse>.Success(response);
+    }
 }
--- src/Gym.Application/Services/Auth/IAuthService.cs
+++ src/Gym.Application/Services/Auth/IAuthService.cs
@@ -1,10 +1,12 @@
 using Gym.Application.Dtos.Auth.Request;
 using Gym.Application.Dtos.Auth.Response;
+using Gym.Application.Dtos.User.Request;
 using Gym.Domain.Abstractions.ResultPattern;
 
 namespace Gym.Application.Services.Auth;
 
 public interface IAuthService
 {
-    Task<Result<LoginResponse>> Login(LoginRequest request, CancellationToken cancellationToken);
+    Task<Result<LoginResponse>> LoginAsync(LoginRequest request, CancellationToken cancellationToken);
+    Task<Result<LoginResponse>> RegisterAsync(CreateUserRequest request, CancellationToken cancellationToken);
 }
