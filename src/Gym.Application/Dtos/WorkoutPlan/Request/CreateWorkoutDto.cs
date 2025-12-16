48cfb606beed17b78e60a503e936ba9706ce856b
Lian Souza <lianmiranda26@gmail.com>
2025-12-15 20:22:17 -0400

    :sparkles: Feat(Workout Plan Dto): Adding validation with data annotations

--- src/Gym.Application/Dtos/WorkoutPlan/Request/CreateWorkoutDto.cs
+++ src/Gym.Application/Dtos/WorkoutPlan/Request/CreateWorkoutDto.cs
@@ -1,14 +1,21 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 using Gym.Domain.Enums;
 
 namespace Gym.Application.Dtos.WorkoutPlan.Request;
 
 public record CreateWorkoutDto
 {
-    [Required] public Guid UserId { get; init; }
+    [Required] [JsonIgnore] public Guid UserId { get; set; }
     [Required] public string Name { get; init; } = null!;
     public string? Description { get; init; }
+
+    [JsonPropertyName("days_per_week")]
+    [Range(1, 7, ErrorMessage = "{0} must be between {1} and {2}")]
     public sbyte? DaysPerWeek { get; init; }
+
+    [Range(1, 12, ErrorMessage = "{0} must be between {1} and {2}")]
     public sbyte? Months { get; init; }
+
     public Goal? Goal { get; init; }
 }
